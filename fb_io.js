7587071a0dd7753e7ea58b9def3a32ec297def3f
23209zl <23209zl@hvhs.school.nz>
2026-05-18 23:54:58 +0000

    I made a change

--- fb_io.js
+++ fb_io.js
@@ -6,14 +6,27 @@
  **                                                          **
  **************************************************************
  **************************************************************/
-function fb_authenticate(){
+
+var GLOBAL_user
+
+function fb_authenticate() {
+ if(_user){
+    console.log("User Is Logged In")
+    GLOBAL_user = _user;
+  }else{
+    console.log("User Is NOT Logged In - Starting the popup process")
+    fb_popupLogin();
+  }
+}
+
+function fb_popupLogin(){
   var provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then((result) => {
   GLOBAL_user = result.user; 
   console.log("User has logged in")
   });
 }
-
-function fb_error(){
+  
+  function fb_error() {
     // Don't forget your error handling!
-}
+  }
