46604f48c95033dcfd06bab393fa8517db4f0ddc
23209zl <23209zl@hvhs.school.nz>
2026-05-19 00:20:05 +0000

    I made a change

---
+++ fb_setup.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDr_w7idgxVzZCoSgAfyJrE19q9x86cozY",
+  authDomain: "jackson-li-12comp.firebaseapp.com",
+  databaseURL: "https://jackson-li---sals-strawberries-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "Jackson Li - Sals Strawberries",
+  storageBucket: "Jackson Li - Sals Strawberries.firebasestorage.app",
+  messagingSenderId: "615513125295",
+  appId: "1:615513125295:web:4a5a7af46b468ab019ff5a"
+  };
--- script.js
+++ script.js
@@ -4,4 +4,10 @@ console.log("Running Sal's Strawberries")
 function writeForm(){
     // Get the form data
     const favoriteFruit = document.getElementById("favoriteFruit").value;
+    const name = document.getElementById('name').value;
+    const fruitQuantity = document.getElementById('fruitQuantity').value;
+
+    firebase.database().ref('/').set(
+    
+    )
 }
